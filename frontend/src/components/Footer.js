fbacc53f2c726faf55cf1f43e306d8fa01edb6fb
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-02-11 09:20:32 +0530

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center p-3 mt-4">
+      OctoImpact © 2026 | Built for Octopus Hackathon
+    </footer>
+  );
+}
+
+export default Footer;
