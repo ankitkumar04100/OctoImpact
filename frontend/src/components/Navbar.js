b2c7dcbc32955be5dcfa9c55847d4aa105dd7300
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-02-11 09:18:31 +0530

    Update and rename Navbar.js to frontend/src/components/Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">OctoImpact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
